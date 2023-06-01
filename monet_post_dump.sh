0c75c1f309434f78f50e6e84584a6dd9838c7538
Muriki G. Yamanaka <64929739+MurikiGY@users.noreply.github.com>
2023-06-01 11:46:14 -0300

    Update monet_post_dump.sh

--- monet_post_dump.sh
+++ monet_post_dump.sh
@@ -13,28 +13,28 @@ then
 else
 	file=$1
 
-	#Remove sys, set schema and sequences
-	sed 's/"sys".//g' $file | grep -v -E 'SET SCHEMA|CREATE SEQUENCE|ALTER TABLE' > tmp
+	#Remove sys, set schema and sequences and coments
+	sed 's/"sys".//g' $file | grep -v -E 'SET SCHEMA|CREATE SEQUENCE|ALTER TABLE|--' > tmp
 
-	#Remove coments, foreign keys from dump file and adjust types
-	grep -v -E 'FOREIGN KEY|--' tmp         | sed ':a;N;$!ba;s/,\n);/\n);/g'    | sed 's/"//g' |
-	sed 's/TINYINT/SMALLINT/g'              | sed 's/DOUBLE/DOUBLE PRECISION/g' |
-	sed 's/CHARACTER LARGE OBJECT/TEXT/g'   | sed 's/HUGEINT/BIGINT/g' > create_tables.sql
+	#Remove primary and foreign keys from dump file and adjust types
+	grep -v -E 'FOREIGN KEY|PRIMARY KEY' tmp  | sed ':a;N;$!ba;s/,\n);/\n);/g'    | sed 's/"//g' |
+	sed 's/TINYINT/SMALLINT/g'                | sed 's/DOUBLE/DOUBLE PRECISION/g' |
+	sed 's/CHARACTER LARGE OBJECT/TEXT/g'     | sed 's/HUGEINT/BIGINT/g' > create_tables.sql
 
-	#Generate the file of foreign keys creation
-	grep -E 'CREATE TABLE|FOREIGN KEY|);' tmp  | sed -z 's/\n);/;/g' | sed 's/CREATE/ALTER/g' |
+	#Generate the file of restrictions
+	grep -E 'CREATE TABLE|CONSTRAINT|);' tmp   | sed -z 's/\n);/;/g' | sed 's/CREATE/ALTER/g' |
 	sed -z 's/\n\tCONSTRAINT/\tCONSTRAINT/g'   | grep 'CONSTRAINT'   |
 	sed -z 's/\tCONSTRAINT/\n\tCONSTRAINT/g'   | sed -z 's/(\n/\n/g' |
-	sed 's/CONSTRAINT/ADD CONSTRAINT/g' > foreign_key.sql
+	sed 's/CONSTRAINT/ADD CONSTRAINT/g' > restrictions.sql
 
 	#Generate temp file to export from monet and import to postgres
 	data_path='\/home\/banco\/dbteam\/simcaq_database\/'
 	mkdir -p /home/banco/dbteam/simcaq_database/
 	grep -v -E 'CONSTRAINT|START TRANSACTION|COMMIT' create_tables.sql  |
-	sed ':a;N;$!ba;s/,\n);/\n);/g' 	| sed 's/NOT NULL//g'           |
-	sed 's/INTEGER//g'              | sed 's/DOUBLE PRECISION//g'   |
-	sed 's/SMALLINT//g'             | sed 's/BOOLEAN//g'            |
-	sed 's/BIGINT//g'               | sed 's/TEXT//g'               |
+	sed ':a;N;$!ba;s/,\n);/\n);/g' 	| sed 's/NOT NULL//g'               |
+	sed 's/INTEGER//g'              | sed 's/DOUBLE PRECISION//g'       |
+	sed 's/SMALLINT//g'             | sed 's/BOOLEAN//g'                |
+	sed 's/BIGINT//g'               | sed 's/TEXT//g'                   |
 	sed 's/VARCHAR(.*)//g'          | sed 's/DECIMAL(.*)//g' > tmp2
 
 	#Generate export file
@@ -57,6 +57,3 @@ else
 	rm tmp
 	rm tmp2
 fi
-
-
-
