8336b51e0625af49c77add37fb29fd462196c4e1
Muriki <mgy20@inf.ufpr.br>
2024-10-09 12:05:13 -0300

    Transformado codigo de predicados em função

--- verilog/main.cpp
+++ verilog/main.cpp
@@ -5,23 +5,9 @@
 
 using namespace std;
 
-int main (int argc, char *argv[]) {
-  int leng;
-  vector<string> numeric;
-  vector<string> categoric;
-
-  cin >> leng;
-  for (int i=0; i<leng ;i++){
-    string line, att, type;
-    cin >> line;
-    att = line.substr(0, line.find(";"));
-    type = line.substr(line.find(";")+1, line.length());
-    if (type == "numeric")
-      numeric.push_back(att);
-    else if (type == "categoric")
-      categoric.push_back(att);
-  }
-
+// Generate the predicates using the attributes
+// The predicates are only between attributes with the same domain
+void gen_predicates(vector<string> numeric, vector<string> categoric){
   for (int i=0; i<numeric.size() ;i++)
     for (int j=i; j<numeric.size() ;j++){
       cout << numeric[i] << " = " << numeric[j] << endl;
@@ -39,8 +25,26 @@ int main (int argc, char *argv[]) {
       cout << categoric[i] << " = " << categoric[j] << endl;
       cout << categoric[i] << " != " << categoric[j] << endl;
     }
+}
+
+int main (int argc, char *argv[]) {
+  int leng;
+  vector<string> numeric;
+  vector<string> categoric;
+
+  // Read attributes
+  cin >> leng;
+  for (int i=0; i<leng ;i++){
+    string line, att, type;
+    cin >> line;
+    att = line.substr(0, line.find(";"));
+    type = line.substr(line.find(";")+1, line.length());
+    if (type == "numeric") { numeric.push_back(att); }
+    else if (type == "categoric") { categoric.push_back(att); }
+  }
+
+  gen_predicates(numeric, categoric);
 
 
-  
   return 0;
 }
