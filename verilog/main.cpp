717463d2829034d5579445e5589e6c0a3c736321
Dante Eleuterio <danteeleuteiro00@gmail.com>
2024-10-14 14:15:13 -0300

    build pli done

--- verilog/main.cpp
+++ verilog/main.cpp
@@ -27,57 +27,96 @@ void gen_predicates(vector<string> numeric, vector<string> categoric){
     }
 }
 
-vector<map<string,string>> build_table(string table_file){
-  int leng;
-  ifstream file(table_file);
-  string line;
-  vector<string> headers;
-  vector<map<string,string>> data;
-  if (getline(file, line)) {
+map<string,vector<string>> build_table(string table_file){
+    ifstream file(table_file);
+    string line;
+    vector<string> headers;    
+    map<string, vector<string>> data;  
+
+    if (getline(file, line)) {
         stringstream headerStream(line);
         string header;
         
         while (getline(headerStream, header, ',')) {
             headers.push_back(header);
+            data[header] = vector<string>();  
         }
     }
-  
-  while (getline(file, line)) {
+
+    while (getline(file, line)) {
         stringstream lineStream(line);
         string cell;
-        map<string, string> rows;   
-        size_t i = 0;
+        int i = 0;
+
         while (getline(lineStream, cell, ',')) {
             if (i < headers.size()) {
-                rows[headers[i]] = cell;  
+                data[headers[i]].push_back(cell);  
             }
             i++;
         }
-        data.push_back(rows);
     }
+
     file.close();
 
-  for (const auto& row : data) {
-      for (const auto& pair : row) {
-          cout << pair.first << ": " << pair.second << " | ";  
-      }
-      cout << endl;
-  }
-  cout<<endl;
-  return data;
-
+    // for (const auto& pair : data) {
+    //     cout << "Attribute: " << pair.first << " -> ";
+    //     for (const auto& value : pair.second) {
+    //         cout << value << " ";
+    //     }
+    //     cout << endl;
+    // }
+    return data;
 }
 
-
+void build_pli(vector<string> numeric, vector<string> categoric,map<string,PLI<string>> categoric_plis,map<string,PLI<int>> numeric_plis,map<string,vector<string>> data){
+  int index=0;
+  for (const auto& pair : data) {
+    for (const auto& value : pair.second) {
+      auto it = find(numeric.begin(),numeric.end(),pair.first);
+      if (it != numeric.end()){
+        numeric_plis[pair.first].clusters[stoi(value)].push_back(index);
+        index++;
+      }
+      else{
+        it = find(categoric.begin(),categoric.end(),pair.first);
+        if (it != categoric.end()){
+          categoric_plis[pair.first].clusters[value].push_back(index);
+          index++;
+        }
+      }
+    }
+    index=0;
+  } 
+  for(const auto& pair: numeric_plis){
+    cout << pair.first << ":" << endl;
+    for (const auto& key : pair.second.clusters) {
+        cout << key.first << "->";
+      for (const auto& value : key.second) {
+        cout << value << "|";
+    }
+    cout << endl;
+    }
+  }
+  for(const auto& pair: categoric_plis){
+    cout << pair.first << ":" << endl;
+    for (const auto& key : pair.second.clusters) {
+        cout << key.first << "->";
+      for (const auto& value : key.second) {
+        cout << value << "|";
+    }
+    cout << endl;
+    }
+  }
+}
 
 
 int main (int argc, char *argv[]) {
   int leng;
   vector<string> numeric;
   vector<string> categoric;
-  vector<PLI<string>> categoric_plis;
-  vector<PLI<int>> numeric_plis;
-  vector<map<string,string>> data;
+  map<string,PLI<string>> categoric_plis;
+  map<string,PLI<int>> numeric_plis;
+  map<string,vector<string>> data;
   // Read attributes
   cin >> leng;
   for (int i=0; i<leng ;i++){
@@ -85,22 +124,14 @@ int main (int argc, char *argv[]) {
     cin >> line;
     att = line.substr(0, line.find(";"));
     type = line.substr(line.find(";")+1, line.length());
-    if (type == "numeric") { 
-      PLI<int> pli_auxn;
-      pli_auxn.att=att;
+    if (type == "numeric") 
       numeric.push_back(att); 
-      numeric_plis.push_back(pli_auxn);  
-      }
-    else if (type == "categoric") { 
-      PLI<string> pli_auxc;
-      pli_auxc.att=att;
+    else if (type == "categoric")
       categoric.push_back(att);
-      categoric_plis.push_back(pli_auxc);
-      }
   }
 
   gen_predicates(numeric, categoric);
   data = build_table("table.csv");
-
+  build_pli(numeric,categoric,categoric_plis,numeric_plis,data);
   return 0;
 }
